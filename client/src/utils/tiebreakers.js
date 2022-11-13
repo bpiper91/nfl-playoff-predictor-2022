4b95ec39c9720cae25990bb12125aecdf15529b4
bpiper91 <bpiper91@gmail.com>
2022-11-13 16:20:47 -0500

    added additional tiebreaker rules text

---
+++ client/src/utils/tiebreakers.js
@@ -0,0 +1,69 @@
+// NFL Tie-breaking procedures
+
+// WITHIN A DIVISION
+
+// Two Teams
+
+// 1. Head to Head Record
+// 2. Division Record
+// 3. Common Games Record
+// 4. Conference Record
+// 5. Strength of Victory
+// 6. Strenth of Schedule
+// 7. Combined Conference Ranking in PF and PA
+// 8. Combined Leage Ranking in PF and PA
+// 9. Net Points in Common Games
+// 10. Net Points in All Games
+// 11. Net TDs in All Games
+// 12. Coin Flip 
+
+// Three Or More Teams
+
+// If two teams are tied after one or more are eliminated, use 2-team format above
+
+// 1. Head to Head Record
+// 2. Division Record
+// 3. Common Games Record
+// 4. Conference Record
+// 5. Strength of Victory
+// 6. Strenth of Schedule
+// 7. Combined Conference Ranking in PF and PA
+// 8. Combined Leage Ranking in PF and PA
+// 9. Net Points in Common Games
+// 10. Net Points in All Games
+// 11. Net TDs in All Games
+// 12. Coin Flip 
+
+// FOR THE WILDCARD
+
+// Two Teams
+
+// 1. Head to Head Record (if applicable)
+// 2. Conference Record
+// 3. Common Games Record (minimum of 4 games)
+// 4. Strength of Victory
+// 5. Strength of Schedule
+// 6. Combined Conference Ranking in PF and PA
+// 7. Combined League Ranking in PF and PA
+// 8. Net Points in Conference Games
+// 9. Net Points in All Games
+// 10. Net TDs in All Games
+// 11. Coin Flip
+
+// Three or More Teams
+
+// If two teams remain tied, restart at step 1 of the 2-team format above
+// If three remain after a 4th is eliminated, restart at step 2 below.
+
+// 1. Division tiebreaker to eliminate all but the highest ranked team in each division
+// 2. Head to Head Sweep (if applicable - beat or lost to all others)
+// 3. Conference Record
+// 4. Common Games Record (minimum 4 games)
+// 5. Strength of Victory
+// 6. Strength of Schedule
+// 7. Combined Conference Ranking in PF and PA
+// 8. Combined League Ranking in PF and PA
+// 9. Net Points in Conference Games
+// 10. Net Points in All Games
+// 11. Net TDs in All Games
+// 12. Coin Flip
